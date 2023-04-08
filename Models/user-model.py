c2bfffcdfac913cc02cb2f6e7632e4023d9d081d
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-08 23:11:31 +0200

    add configs

--- Models/user-model.py
+++ Models/user-model.py
@@ -1,3 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+db = SQLAlchemy()
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
