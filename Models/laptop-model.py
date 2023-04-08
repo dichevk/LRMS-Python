6348b60eaaae0a34a0a272cb8cde567617a4682b
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-08 22:57:09 +0200

    add simple user model

--- Models/laptop-model.py
+++ Models/laptop-model.py
@@ -34,3 +34,12 @@ class Laptop(db.Model):
         'price': self.price,
         'specs': self.specs
         }
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
