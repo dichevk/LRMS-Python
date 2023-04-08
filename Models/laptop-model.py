c9580baca19cebbbaf134e258b06f888af5b52bd
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-08 23:11:04 +0200

    refactor user model

--- Models/laptop-model.py
+++ Models/laptop-model.py
@@ -35,27 +35,3 @@ class Laptop(db.Model):
         'specs': self.specs
         }
     
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.name}', '{self.email}')"
-    
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def to_dict(self):
-        return {
-        'id': self.id,
-        'name': self.name,
-        'email': self.email,
-        'password': self.password,
-        }
---
+++ Models/user-model.py
@@ -0,0 +1,24 @@
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'email': self.email,
+        'password': self.password,
+        }
