c5837e13e1ed160d46dcac2a8006bbd41431b2ed
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-19 23:40:01 +0200

    date property addition and init function

--- Models/laptop_model.py
+++ Models/laptop_model.py
@@ -9,10 +9,18 @@ class Laptop(db.Model):
     image = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
     specs = db.Column(db.String(500), nullable=False)
-    availability_start_date = db.Column(db.String(500), nullable=False)
-    availability_end_date = db.Column(db.String(500), nullable=True)
+    availability_start_date = db.Column(db.Date, nullable=False)
+    availability_end_date = db.Column(db.Date, nullable=True)
     available = db.Column(db.Boolean, nullable=False)
 
+    def __init__(self, name, image, price, specs, availability_start_date, availability_end_date=None, available=True):
+        self.name = name
+        self.image = image
+        self.price = price
+        self.specs = specs
+        self.availability_start_date = availability_start_date
+        self.availability_end_date = availability_end_date
+        self.available = available
     @classmethod
     def get_all(cls):
         return cls.query.all()
@@ -35,6 +43,9 @@ class Laptop(db.Model):
         'name': self.name,
         'image': self.image,
         'price': self.price,
-        'specs': self.specs
+        'specs': self.specs,
+        'availability_start_date':self.availability_start_date,
+        'availability_end_date':self.availability_end_date,
+        'available':self.available
         }
     
