2f7ef59bc32fc3e5267450e334800388acdbbee7
KonstantinDichev <kdichev.nl@gmail.com>
2023-05-02 23:48:16 +0200

    add order item model

---
+++ Models/order_item_model.py
@@ -0,0 +1,11 @@
+from app import db
+
+class OrderItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"<OrderItem {self.id}>"
