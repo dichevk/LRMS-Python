f5c2d1debabaa2ea8ea9bce570cfb675b72e1a84
KonstantinDichev <kdichev.nl@gmail.com>
2023-05-05 22:57:01 +0200

    update routes

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
-from .Views import bp
+from .Controllers.cart_controller import cart_bp
+from .Controllers.laptop_controller import laptop_bp
+from .Controllers.order_controller import order_bp
+from .Controllers.review_controller import review_bp
+from .Controllers.user_controller import user_bp
 from .Models import db
 
 def create_app():
@@ -7,5 +11,9 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///laptops.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    app.register_blueprint(bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(laptop_bp)
+    app.register_blueprint(review_bp)
+    app.register_blueprint(order_bp)
+    app.register_blueprint(cart_bp)
     return app
